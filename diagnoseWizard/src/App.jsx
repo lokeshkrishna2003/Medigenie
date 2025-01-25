98ff42f96ddfc5e39926d1bcaf21c3df0645de1b
Dama Venkata Vamsi <venkatavamsi560@gmail.com>
2025-01-25 10:32:40 +0530

    added all files

---
+++ diagnoseWizard/src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './Components/Navbar'
+import Hero from './Pages/Hero'
+import LoginPage from './Pages/LoginPage'
+import SignUpPage from './Pages/SignUpPage'
+import ChangePassword from './Pages/ChangePassword'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Route, Routes } from 'react-router-dom'
+import { useRef } from 'react';
+import PageNotFound from './Pages/PageNotFound';
+import Doctors from './Pages/Doctors';
+import DiagnosesPage from './Pages/DiagnosesPage';
+import ForgotPassword from './Pages/ForgotPassword';
+function App() {
+  const HomeSection = useRef(null);
+  const AboutSection = useRef(null);
+  const ServicesPageSection = useRef(null);
+  const WhyChooseUsSection = useRef(null);
+  const CarousalPageSection = useRef(null);
+  const ConnectWithUsSection = useRef(null);
+  const FooterSection = useRef(null);
+  const jwt = sessionStorage.getItem('jwt');
+  const encryptedData=sessionStorage.getItem('encryptedData');
+  return (
+    <div >
+    
+      <Navbar message={"login successful"}
+        AboutSection={AboutSection}
+        HomeSection={HomeSection}
+        ServicesPageSection={ServicesPageSection}
+        WhyChooseUsSection={WhyChooseUsSection}
+        CarousalPageSection={CarousalPageSection}
+        ConnectWithUsSection={ConnectWithUsSection}
+        FooterSection={FooterSection}
+      />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+
+      <Routes>
+        <Route path='/'
+          element={
+            <Hero
+              AboutSection={AboutSection}
+              HomeSection={HomeSection}
+              ServicesPageSection={ServicesPageSection}
+              WhyChooseUsSection={WhyChooseUsSection}
+              CarousalPageSection={CarousalPageSection}
+              ConnectWithUsSection={ConnectWithUsSection}
+              FooterSection={FooterSection}
+            />} />
+        <Route path='/hospitals' element={<Doctors />} />
+        <Route path='/diagnoses' element={<DiagnosesPage />} />
+        <Route path='/forgetPassword' element={<ForgotPassword/>} />
+        {!jwt && <Route path='/login' element={<LoginPage />} />}
+        {!jwt && <Route path='/signup' element={<SignUpPage />} />}
+        {encryptedData && <Route path='/changePass' element={<ChangePassword />} />}
+
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+
+
+    </div>
+  )
+}
+
+export default App;
---
+++ diagnoseWizard/src/Pages/Hero.jsx
@@ -0,0 +1,26 @@
+import Home from "./Home";
+import About from "./About";
+import ServicesPage from "./ServicesPage";
+import WhyChooseUs from "./WhyChooseUs";
+import CarousalPage from "./CarousalPage";
+import ConnectWithUs from "./ConnectWithUs";
+import Footer from "./Footer";
+import { useRef } from 'react'
+
+const Hero = (props) => {
+    const MessageNameSection = useRef(null);
+    return (
+        <section className="w-full">
+            
+            <Home HomeSection={props.HomeSection} />
+            <About AboutSection={props.AboutSection} />
+            <ServicesPage ServicesPageSection={props.ServicesPageSection} />
+            <WhyChooseUs WhyChooseUsSection={props.WhyChooseUsSection} />
+            <CarousalPage CarousalPageSection={props.CarousalPageSection} />
+            <ConnectWithUs ConnectWithUsSection={props.ConnectWithUsSection} MessageNameSection={MessageNameSection} />
+            <Footer  ConnectWithUsSection={props.ConnectWithUsSection} MessageNameSection={MessageNameSection}/>
+        </section>
+    );
+}
+
+export default Hero;
